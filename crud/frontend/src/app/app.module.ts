39d1d8f2a31e80ed7c8faa7ab39896340deb462e
Alécio Godoi <alecio.godoi@ymail.com>
2020-06-18 09:22:22 -0300

    Commit inicial CRUD

---
+++ crud/frontend/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { NavComponent } from './template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
+import { CursoCrudComponent } from './views/curso-crud/curso-crud.component';
+import { CursoCreateComponent } from './components/curso/curso-create/curso-create.component';
+import { CursoReadComponent } from './components/curso/curso-read/curso-read.component';
+import { CursoUpdateComponent } from './components/curso/curso-update/curso-update.component';
+import { CursoDeleteComponent } from './components/curso/curso-delete/curso-delete.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    CursoCrudComponent,
+    CursoCreateComponent,
+    CursoReadComponent,
+    CursoUpdateComponent,
+    CursoDeleteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
